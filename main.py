46396f488718f255819f34152d9e04c32196c007
Lokee86 <otstructures@gmail.com>
2024-08-08 13:05:52 -0700

    adjusted data handling, fixed indenting in one function

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ from functools import lru_cache
 @lru_cache
 def read_book():
     #path argument is globalized in main()
-    with open(path) as f:
-        book = f.read()
-        return book
+    global file_name
+    try:
+        with open(path) as f:
+            book = f.read()
+            file_name = path.split("/")[-1]
+    except Exception:
+        print("Invalid file path, analyzing entry instead")
+        book = path
+        file_name = "Input String"
+    return book
 
 # obtain and cache counts for line, words, unique words, and the number of title lines present
 @lru_cache
@@ -15,7 +22,7 @@ def obtain_counts():
     lines = read_book().split("\n")
     line_count = len(lines)
     words = len(read_book().split())
-    unique_words = len(set(count_words()))
+    unique_words = len(set(count_words()[0]))
     for line in lines:
         if line.istitle():
             chapters += 1
@@ -52,8 +59,8 @@ def count_words(option = True):
             else:
                 word_counts[stripped_word] += 1
     if option:
-        return sorted(list(word_counts.items()), key=lambda x: x[1], reverse=True)
-    return sorted(list(word_counts.items()), key=lambda x: x[0])
+        return sorted(list(word_counts.items()), key=lambda x: x[1], reverse=True), word_counts
+    return sorted(list(word_counts.items()), key=lambda x: x[0]), word_counts
 
 # sorts and caches counted characters into seperate lists of numbers, letters,
 # ASCII characters, and invisible characters (spaces and newlines)
@@ -99,63 +106,73 @@ def clear_cache():
 
 # produces an output based on the selected option
 def print_data(option = True):
-        print(f"---------------Beginning Report of {file_name}-------------------------")
-        print(f"There are {obtain_counts()[0]} lines of text, {obtain_counts()[1]} words\n{obtain_counts()[2]} unique words, and {obtain_counts()[3]} title lines\nin the document.")
-        match option:
-            case 1:
-                for letter in character_lists(option)[0]:
-                    print(f"The uppercase letter '{letter[0]}' appears {letter[1]} times.")
-            case 2:
-                for letter in character_lists(option)[0]:
-                    print(f"The uppercase letter '{letter[0]}' appears {letter[1]} times.")
-            case 3:
-                for number in character_lists(option)[2]:
-                    print(f"The number '{number[0]}' appears {number[1]} times.")
-            case 4:
-                for char in character_lists(option)[3]:
-                    print(f"The character '{char[0]}' appears {char[1]} times.")
-            case 5:
-                for char in character_lists(option)[4]:
-                    if char[0] == " ":
-                        print(f"There are {char[1]} spaces.")
-                    else:
-                        print(f"There are {char[1]} newline characters or returns.")
-            case 6:
-                for word in count_words():
-                    if word[0] == "i":
-                        print(f"The word 'I' was used {word[1]} times.")
-                    else:
-                        print(f"The word '{word[0]}' was used {word[1]} times.")
-            case 7:
-                for letter in all_letter_counts(option):
+    read_book()        
+    print(f"-----------------Beginning Report of {file_name}-----------------------")
+    print(f"There are {obtain_counts()[0]} lines of text, {obtain_counts()[1]} words\n{obtain_counts()[2]} unique words, and {obtain_counts()[3]} title lines\nin the document.")
+    match option:
+        case 1:
+            for letter in character_lists()[0]:
+                print(f"The uppercase letter '{letter[0]}' appears {letter[1]} times.")
+        case 2:
+            for letter in character_lists()[0]:
+                print(f"The uppercase letter '{letter[0]}' appears {letter[1]} times.")
+        case 3:
+            for number in character_lists()[2]:
+                print(f"The number '{number[0]}' appears {number[1]} times.")
+        case 4:
+            for char in character_lists()[3]:
+                print(f"The character '{char[0]}' appears {char[1]} times.")
+        case 5:
+            for char in character_lists()[4]:
+                if char[0] == " ":
+                    print(f"There are {char[1]} spaces.")
+                else:
+                    print(f"There are {char[1]} newline characters or returns.")
+        case 6:
+            for word in count_words()[0]:
+                if word[0] == "i":
+                    print(f"The word 'I' was used {word[1]} times.")
+                else:
+                    print(f"The word '{word[0]}' was used {word[1]} times.")
+        case 7:
+            for letter in all_letter_counts(option):
+                print(f"The letter '{letter[0]}' appears {letter[1]} times.")
+        # Code is repeated here, because I determined it would take the same amount of code to avoid repeating it.
+        case 8:
+            for letter in character_lists()[0]:
                     print(f"The letter '{letter[0]}' appears {letter[1]} times.")
-            # Code is repeated here, because I determined it would take the same amount of code to avoid repeating it.
-            case 8:
-                for letter in character_lists(option)[0]:
-                    print(f"The letter '{letter[0]}' appears {letter[1]} times.")
-                for number in character_lists(option)[2]:
+            for number in character_lists()[2]:
                     print(f"The number '{number[0]}' appears {number[1]} times.")
-                for char in character_lists(option)[3]:
+            for char in character_lists()[3]:
                     print(f"The character '{char[0]}' appears {char[1]} times.")
-                for char in character_lists(option)[4]:
-                    if char[0] == " ":
-                        print(f"There are {char[1]} spaces.")
-                    else:
-                        print(f"There are {char[1]} newline characters or returns.")
-                for word in count_words():
-                    if word[0] == "i":
-                        print(f"The word 'I' was used {word[1]} times.")
-                    else:
-                        print(f"The word '{word[0]}' was used {word[1]} times.")
-            case _:
+            for char in character_lists()[4]:
+                if char[0] == " ":
+                    print(f"There are {char[1]} spaces.")
+                else:
+                    print(f"There are {char[1]} newline characters or returns.")
+            for word in count_words()[0]:
+                if word[0] == "i":
+                    print(f"The word 'I' was used {word[1]} times.")
+                else:
+                    print(f"The word '{word[0]}' was used {word[1]} times.")
+        case _:
                 print("Invalid Option, Please Choose a Valid Option.")
                 return
+    print("---------------------------End of Report--------------------------------")
+
+def output_file():
+    pass
+
+def search(term):
+    if term in count_words()[1]:
+        print(f"The word '{term}' occurs {count_words()[1][term]} times.")
+    else:
+        print(f"The word '{term}' could not be found.")
 
 def main():
-    global path, file_name
+    global path
     path = "books/frankenstein.txt"
-    # path = input("Enter Path:").replace("\\", "/")
-    file_name = path.split("/")[-1]
-    print(all_letter_counts())
+    #path = input("Enter Path:").replace("\\", "/")
+    print_data(1)
 
 main()
